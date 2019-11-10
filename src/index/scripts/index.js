84b8af4db3b307accd612155d8802884b6e7bdd5
ahmad-666 <ahmadrezagghz666@gmail.com>
2019-11-09 17:26:30 -0800

    after cache-then-net

--- src/index/scripts/index.js
+++ src/index/scripts/index.js
@@ -1,34 +1,64 @@
-function showPrompt(evt){
-  promptEvent.prompt() ; 
-  //show prompt for both android and desktop
+let dynamicContentCacheName = 'dynamic-content-v3';
+let networkDataFetched = false ;
+let cardsWrapper = document.querySelector('.cards') ;
+function showPrompt(evt) {
+  promptEvent.prompt();
   promptEvent.userChoice
-  .then(choice=>{
-    //console.log(choice.outcome) ;
-    if(choice.outcome == 'dismissed') console.error('user not add app to home screen') ;
-    else console.log('user add our app to home screen') ;  
-  })
+    .then(choice => {
+      if (choice.outcome == 'dismissed') console.error('user not add app to home screen');
+      else console.log('user add our app to home screen');
+    })
 }
-document.querySelector('#createCard').addEventListener('click',fetchCard);
-function fetchCard(e){
+document.querySelector('#createCard').addEventListener('click', fetchCard);
+//cache-then-network
+function fetchCard(e) {
+  let imgUrl = '/src/assets/imgs/img2.jpg';
+  let txtUrl = 'https://jsonplaceholder.typicode.com/users' ;
   let fetchConfig = {
     method: 'GET',
     mode: 'cors'
   }
-  fetch('https://jsonplaceholder.typicode.com/users',fetchConfig)
-  .then(res=>res.json())
-  .then(data=>{
-    createCard('/src/assets/imgs/img1.jpg',data[0].name) ;
+  fetch(txtUrl,fetchConfig) //when we have net 
+  .then(res => res.json())
+  .then(data => {
+    networkDataFetched = true ;
+    clearCards() ;
+    createCard(imgUrl,data[0].name);
+    //we should call createCard() two times one for fetch and one for cache
+    //because we want to update UI totally(get new data)
   })
-  .catch(msg=>console.error(msg))
+  .catch(msg => console.error(msg))
+  //fetch and reading from cache are both async so there is no difference 
+  //that first we use fetch then read from cache or reverse.
+  if('caches' in window){ //it should be totally separate from 'fetch'
+    caches.match(txtUrl)
+    //if we dont have 'txtUrl' we still execute .then here 
+    .then(cacheResp=>{
+      if(cacheResp) return cacheResp.json() ;  
+    })
+    .then(data=>{
+      if(!networkDataFetched) {
+        //because its async code we need to place this 'if' exactly here and if we place in inside top 'then' can cause some problems
+        clearCards() ;
+        createCard(imgUrl,data[0].name);
+      }
+    })
+    .catch(()=>{;})
+  }
 }
-function createCard(imgSrc,text){
+function clearCards(){
+  while(cardsWrapper.hasChildNodes()){
+    cardsWrapper.removeChild(cardsWrapper.lastChild) ;
+  }
+}
+function createCard(imgSrc, text) {
   let card = document.createElement('div');
-  card.classList.add('card') ;
-  let img = document.createElement('img') ;
-  img.setAttribute('src',imgSrc) ;
-  let p = document.createElement('p') ;
-  p.textContent = text ;
+  card.classList.add('card');
+  let img = document.createElement('img');
+  img.setAttribute('src', imgSrc);
+  let p = document.createElement('p');
+  p.textContent = text;
   card.appendChild(img);
   card.appendChild(p);
-  document.body.appendChild(card) ;
+  cardsWrapper.appendChild(card);
 }
--- src/sw.js
+++ src/sw.js
@@ -1,20 +1,11 @@
-let staticCacheName = 'static-v13' ;
-let dynamicCacheName = 'dynamic-v13' ;
-//each time we change something inside sw.js then new sw will be installed 
-//but if we change something outside of sw then we still read from cache(old files)
-//so we need to change cache version
+let staticCacheName = 'static-v17' ;
+let dynamicCacheName = 'dynamic-v17' ;
+let offlineFallback = '/src/404.html' ;
 let staticCachingAssets = [
     '/src/',
-    //key names must match exact url here e.g if we open our site via http://127.0.0.1:3500/src/
-    //then caching just '/src/index.html' is not enough(we can still open http://127.0.0.1:3500/src/index.html)
-    //but not http://127.0.0.1:3500/src/ so we need to cache '/src/' manually too 
-    //if we open site via http://127.0.0.1:3500/ we need only to cache '/'
     '/src/index.html',
-    //if dont add .html files then caching any .css,.js,images,... is nonsense because first we must read 
-	//.html file and then read other assets from it 
+    offlineFallback ,
     '/src/libraries/fontawesome/all.min.css',
-    //there is no need to always work with '/' to point to root folder and something like :
-	//cache.add('../src/') it true too 
     '/src/framework.css',
     '/src/init.css', 
     '/src/index/styles/index.css',
@@ -22,17 +13,10 @@ let staticCachingAssets = [
     '/src/assets/icons/icon-144x144.png',
     '/src/assets/imgs/img1.jpg',
     '/src/assets/fonts/iransans.ttf'
-    //there is no need to cache fonts really but we should know that sometimes even when we cache something we still 
-    //have problem with it and its because inside that caching file we load other assets(like fontAwesome)
-    //for these times we can use dynamic caching.
 ]
 self.addEventListener('install',function(evt){
     console.log('sw has been installed',evt) ;
     evt.waitUntil(
-        //inside sw we always work with async code and sometimes we need to be sure we perform some action 
-        //and then we want to perform new action so we can access to <evt>.waitUntil(...) and here we will be sure that 
-        //first we cache static assets then we activate sw and do other things. 
-		//we pass our code as arg inside 'waitUntil' not as function(same for <evt>.respondWith)
         caches.open(staticCacheName)
         .then(cache=>{
             cache.addAll(staticCachingAssets)   
@@ -51,38 +35,30 @@ self.addEventListener('activate',function(evt){
         })
         .catch(msg=>console.error(msg))
     )
-    return self.clients.claim() ; //make sure our code wont fail 
+    return self.clients.claim() ; 
 });
+//cache-then-network
 self.addEventListener('fetch',function(evt){
-    //we can control(intercept) any fetch events(get/post) and assets here 
-    //we can block certain fetch events,intercept them and change them,...
+    //we should know that pre-caching has nothing to do with 'fetch' 
+    //event and its all about 'install' event 
+    //inside 'fetch' event we will say respond each request with what 
+    //evt.request is http-request object
+    //we should be careful that we should not just store something inside cache and we must return its response too
     evt.respondWith(
-        //we need to check if we have certain asset inside cache or not 
-        caches.match(evt.request)
-        //when we want to send http request we can use both string url or http request object like inside fetch function 
-		//but when we want to retrieve or check something we can only work with http request object(evt.request)
-		//so caches.match(<str>) is wrong 
-        .then(cacheResp=>{
-            //even if we dont find match we still execute .then but cacheResp is empty 
-            if(cacheResp){//find request inside cache
-                return cacheResp ; //retrieve from cache 
-            }
-            else{//does not find request inside cache
-                return fetch(evt.request) //retrieve from net 
-                .then(res=>{
-                    return caches.open(dynamicCacheName)
-                    .then(cache=>{
-                        cache.put(evt.request.url,res.clone()) ;
-                        //now anything that its not inside cache will be caching too
-                        //with interact with app we fetch more things with dynamic caching 
-                        console.log(res) ;
-                        return res ;
-                        //if we dont return then we just put req inside dynamic caching and dont return anythong to user
-                    })
-                })
-                .catch((msg)=>{console.error(msg) ;})//for catch any error
-            }
+        caches.open(dynamicCacheName)
+        .then(cache=>{
+            return fetch(evt.request)
+            .then(res=>{
+                //res is http-response object 
+                cache.put(evt.request.url,res.clone()) ;
+                //now we add anything even pre-cached-static files to 
+                //dynamic cache and we can add some filtering here 
+                return res ;    
+            })       
         })
+        .catch(msg=>console.log('cache cant be open'))   
     );
 })
+
+
 
